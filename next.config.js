251303e4c03cddbd9f6aa81456063b195f28c590
DeshDeepakKant <deshdeepakkant@gmail.com>
2024-02-17 12:00:00 +0530

    Fix deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,17 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  basePath: '/React-D3-VizHub-Interactive-Data-Insights',
+  assetPrefix: '/React-D3-VizHub-Interactive-Data-Insights/',
+  images: {
+    unoptimized: true,
+    loader: 'akamai',
+    path: '',
+  },
   compiler: {
     styledComponents: true
-  }
+  },
+  trailingSlash: true
 }
+
+module.exports = nextConfig
---
+++ src/utils/path.js
@@ -0,0 +1,6 @@
+export const getImagePath = (path) => {
+  const basePath = process.env.NODE_ENV === 'production' 
+    ? '/React-D3-VizHub-Interactive-Data-Insights' 
+    : '';
+  return `${basePath}${path}`;
+}
