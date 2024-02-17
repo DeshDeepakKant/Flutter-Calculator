2b407fa115886b81f3da3a70c3bb7d04b7dffafd
DeshDeepakKant <deshdeepakkant@gmail.com>
2024-02-17 12:00:00 +0530

    Remove .babelrc and add prop-types

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   compiler: {
     styledComponents: true
   },
   reactStrictMode: true
 }
-
-module.exports = nextConfig
