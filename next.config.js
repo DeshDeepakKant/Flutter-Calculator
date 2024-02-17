197edab7f666e92899a88d8e37120c04926482fd
DeshDeepakKant <deshdeepakkant@gmail.com>
2024-02-17 12:00:00 +0530

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/React-D3-VizHub-Interactive-Data-Insights',
+  images: {
+    unoptimized: true,
+  },
   compiler: {
     styledComponents: true
   },
