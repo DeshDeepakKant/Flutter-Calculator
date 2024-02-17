aef96cada01968da5d51f2237b4f77b97ace6972
DeshDeepakKant <deshdeepakkant@gmail.com>
2024-02-17 12:00:00 +0530

    Clean build configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   compiler: {
     styledComponents: true
   },
   reactStrictMode: true
 }
+
+module.exports = nextConfig
