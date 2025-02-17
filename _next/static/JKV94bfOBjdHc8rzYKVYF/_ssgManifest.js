58fe930e0a0145b5337319e7bd5093ec00b801c8
DeshDeepakKant <deshdeepakkant@gmail.com>
2025-02-17 18:48:40 +0530

    Updates

---
+++ _next/static/JKV94bfOBjdHc8rzYKVYF/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fliteracy-rates-by-country","\u002Fkorea-crop-production","\u002Fcovid19-dashboard","\u002Fmost-populated-countries"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
